32f791f878b1c94fa6dbcdaee3dff2b05be2c6ea
josh <josh.cu@gmail.com>
2023-12-03 21:28:18 -0600

    initial package setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros_ppo_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='josh',
+    maintainer_email='josh.cu@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = ros_ppo_controller.controller:main'
+        ],
+    },
+)
