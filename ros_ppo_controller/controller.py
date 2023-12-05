a17ab7b91b64297535850d7830b001a2fc1a950a
josh <josh.cu@gmail.com>
2023-12-05 01:13:41 -0600

    minimise controller code

--- ros_ppo_controller/controller.py
+++ ros_ppo_controller/controller.py
@@ -9,114 +9,59 @@ from ament_index_python.packages import get_package_share_directory
 import numpy as np
 from stable_baselines3 import PPO
 
-model = get_package_share_directory('ros_ppo_controller') + "/models/best_model.zip"
-model = PPO.load(path=model)
-#https://f1tenth-gym.readthedocs.io/en/latest/customized_usage.html#custom-usage
-#https://f1tenth-gym.readthedocs.io/en/latest/api/dynamic_models.html#f110_gym.envs.dynamic_models
-v_max = 20.0
-v_min = -5.0
-speed_reduction = 0.25
+# Constants
+MODEL_PATH = get_package_share_directory('ros_ppo_controller') + "/models/best_model.zip"
+V_MAX = 20.0
+V_MIN = -5.0
+SPEED_REDUCTION = 0.25
 MAX_LIDAR_RANGE = 10.0
-MAX_SPEED = v_max * speed_reduction
-MAX_REVERSE_SPEED = v_min * speed_reduction
+MAX_SPEED = V_MAX * SPEED_REDUCTION
+MAX_REVERSE_SPEED = V_MIN * SPEED_REDUCTION
+MAX_TURN = 3.2 # Range is -3.2 to 3.2
+TIMER_PERIOD = 0.01
 
-MAX_TURN = 3.2 # max turn is -3.2 to 3.2
+# Load the model
+model = PPO.load(path=MODEL_PATH)
 
-def unnormalize_linear_velocity(action):
-    return action * (MAX_SPEED + abs(MAX_REVERSE_SPEED)) + MAX_REVERSE_SPEED
+# Helper functions
+def unnormalize_velocities(actions):
+    linear = actions[1] * (MAX_SPEED + abs(MAX_REVERSE_SPEED)) + MAX_REVERSE_SPEED
+    angular = actions[0] * MAX_TURN
+    return linear, angular
 
-def unnormalize_angular_velocity(action):
-    return action * MAX_TURN
+def clean_scan(scan):
+    return [min(MAX_LIDAR_RANGE, x if x != float('Inf') and not math.isnan(x) else MAX_LIDAR_RANGE) for x in scan]
 
-class ppo_controller(Node):
+class PPOController(Node):
     def __init__(self):
-        # Initialize the publisher
         super().__init__("ppo_controller")
-        self.normalized_scan_cleaned = []
-        self.publisher_ = self.create_publisher(Twist, "cmd_vel", 10)
-        self.scan_subscriber = self.create_subscription(
-            LaserScan,
-            "/scan",
-            self.scan_callback,
-            QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT),
-        )
-        self.odom_subscriber = self.create_subscription(
-            Odometry,
-            "/ego_racecar/odom",
-            self.odom_callback,
-            QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT),
-        )
-        timer_period = 0.01
-        self.pose_saved = ""
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.create_subscription(LaserScan, "/scan", self.scan_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.create_subscription(Odometry, "/ego_racecar/odom", self.odom_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.timer = self.create_timer(TIMER_PERIOD, self.timer_callback)
+        self.normalized_scan = []
+        self.normalized_velocity = -5
         self.cmd = Twist()
-        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-    def scan_callback(self, msg1):
-        # self.get_logger().info('scan: "%s"' % msg1.ranges)
-        scan = msg1.ranges
-        self.scan_cleaned = []
+    def scan_callback(self, msg):
+        cleaned_scan = clean_scan(msg.ranges)
+        self.normalized_scan = [x / MAX_LIDAR_RANGE for x in cleaned_scan]
 
-        # self.get_logger().info('scan: "%s"' % scan)
-        # Assume 360 range measurements
-        for reading in scan:
-            if reading == float("Inf"):
-                self.scan_cleaned.append(MAX_LIDAR_RANGE)
-            elif math.isnan(reading):
-                self.scan_cleaned.append(MAX_LIDAR_RANGE)
-            elif reading > MAX_LIDAR_RANGE:
-                self.scan_cleaned.append(MAX_LIDAR_RANGE)
-            else:
-                self.scan_cleaned.append(reading)
-        
-        # normalize scan to between 0 and 1
-        self.normalized_scan_cleaned = [x / MAX_LIDAR_RANGE for x in self.scan_cleaned]
-
-        # # bodge until model is retrained
-        # original_list = self.normalized_scan_cleaned
-        # desired_length = 1440
-
-        # sampling_interval = len(original_list) / desired_length
-        # self.normalized_scan_cleaned = [original_list[int(i * sampling_interval)] for i in range(desired_length)]
-
-    def odom_callback(self, msg2):
-        # get velocity from odom
-        velocity = msg2.twist.twist.linear.x
-        self.velocity = velocity
-        self.normalized_velocity = velocity / 20.0
+    def odom_callback(self, msg):
+        self.normalized_velocity = msg.twist.twist.linear.x / V_MAX
 
     def timer_callback(self):
-        if len(self.normalized_scan_cleaned) == 0 or self.normalized_velocity == "":
-            return None
-        
-        reversed_scan = self.normalized_scan_cleaned
-        reversed_scan.reverse()
-        #create a numpy array from the normalized velovity prepended to the normalized scan
-        observation = np.array([self.normalized_velocity]+reversed_scan)        
-        action, _states = model.predict(observation, deterministic=True)
-        self.get_logger().info(f"Action: {action}")
-  
-        self.cmd.linear.x = unnormalize_linear_velocity(action[1])  
-        self.cmd.angular.z = unnormalize_angular_velocity(action[0])
-        self.publisher_.publish(self.cmd)
-        
-        
+        if not self.normalized_scan or self.normalized_velocity == -5:
+            return
 
-
-
-
+        observation = np.array([self.normalized_velocity] + self.normalized_scan[::-1])
+        actions, _ = model.predict(observation)
+        self.cmd.linear.x,self.cmd.angular.z = unnormalize_velocities(actions)
+        self.publisher.publish(self.cmd)
 
 def main(args=None):
-    # initialize the ROS communication
     rclpy.init(args=args)
-    # declare the node constructor
-    movement_node = ppo_controller()
-    # pause the program execution, waits for a request to kill the node (ctrl+c)
-    rclpy.spin(movement_node)
-    # Explicity destroy the node
-    movement_node.destroy_node()
-    # shutdown the ROS communication
+    node = PPOController()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
-
-
-if __name__ == "__main__":
-    main()
