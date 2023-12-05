4059552564fc44d0a83a46853918681c12b0c017
josh <josh.cu@gmail.com>
2023-12-05 01:01:03 -0600

    add model to repo

--- ros_ppo_controller/controller.py
+++ ros_ppo_controller/controller.py
@@ -5,16 +5,18 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from rclpy.qos import ReliabilityPolicy, QoSProfile
-
+from ament_index_python.packages import get_package_share_directory
 import numpy as np
 from stable_baselines3 import PPO
-model = "/home/josh/f1tenth_reinforcement_learning/work/best_models/best_model.zip"
+
+model = get_package_share_directory('ros_ppo_controller') + "/models/best_model.zip"
 model = PPO.load(path=model)
 #https://f1tenth-gym.readthedocs.io/en/latest/customized_usage.html#custom-usage
 #https://f1tenth-gym.readthedocs.io/en/latest/api/dynamic_models.html#f110_gym.envs.dynamic_models
 v_max = 20.0
 v_min = -5.0
 speed_reduction = 0.25
+MAX_LIDAR_RANGE = 10.0
 MAX_SPEED = v_max * speed_reduction
 MAX_REVERSE_SPEED = v_min * speed_reduction
 
@@ -22,6 +24,9 @@ MAX_TURN = 3.2 # max turn is -3.2 to 3.2
 
 def unnormalize_linear_velocity(action):
     return action * (MAX_SPEED + abs(MAX_REVERSE_SPEED)) + MAX_REVERSE_SPEED
+
+def unnormalize_angular_velocity(action):
+    return action * MAX_TURN
 
 class ppo_controller(Node):
     def __init__(self):
@@ -55,23 +60,23 @@ class ppo_controller(Node):
         # Assume 360 range measurements
         for reading in scan:
             if reading == float("Inf"):
-                self.scan_cleaned.append(10)
+                self.scan_cleaned.append(MAX_LIDAR_RANGE)
             elif math.isnan(reading):
-                self.scan_cleaned.append(10)
-            elif reading > 10:
-                self.scan_cleaned.append(10)
+                self.scan_cleaned.append(MAX_LIDAR_RANGE)
+            elif reading > MAX_LIDAR_RANGE:
+                self.scan_cleaned.append(MAX_LIDAR_RANGE)
             else:
                 self.scan_cleaned.append(reading)
         
         # normalize scan to between 0 and 1
-        self.normalized_scan_cleaned = [x / 10 for x in self.scan_cleaned]
+        self.normalized_scan_cleaned = [x / MAX_LIDAR_RANGE for x in self.scan_cleaned]
 
-        # bodge until model is retrained
-        original_list = self.normalized_scan_cleaned
-        desired_length = 1440
+        # # bodge until model is retrained
+        # original_list = self.normalized_scan_cleaned
+        # desired_length = 1440
 
-        sampling_interval = len(original_list) / desired_length
-        self.normalized_scan_cleaned = [original_list[int(i * sampling_interval)] for i in range(desired_length)]
+        # sampling_interval = len(original_list) / desired_length
+        # self.normalized_scan_cleaned = [original_list[int(i * sampling_interval)] for i in range(desired_length)]
 
     def odom_callback(self, msg2):
         # get velocity from odom
@@ -89,11 +94,9 @@ class ppo_controller(Node):
         observation = np.array([self.normalized_velocity]+reversed_scan)        
         action, _states = model.predict(observation, deterministic=True)
         self.get_logger().info(f"Action: {action}")
-
-        turn = action[0] * MAX_TURN
-        velocity = unnormalize_linear_velocity(action[1])         
-        self.cmd.linear.x = velocity
-        self.cmd.angular.z = turn
+  
+        self.cmd.linear.x = unnormalize_linear_velocity(action[1])  
+        self.cmd.angular.z = unnormalize_angular_velocity(action[0])
         self.publisher_.publish(self.cmd)
         
         
